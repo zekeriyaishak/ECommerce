554c33a8d72795e9e68c080e3000ef235a73882b
zekishak <zekishakdgny38@gmail.com>
2022-07-18 10:43:56 +0300

    Second Commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+    ProductManager productManager = new ProductManager(new EfProductDal());
+
+    foreach (var product in productManager.GetByUnitPrice(50,100))
+    {
+        Console.WriteLine(product.ProductName);
+    }
---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,65 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            
+            using (NorthwindContext context= new NorthwindContext())
+            {
+                //veri tabanında productın birini  bir entitye eşitle
+                var addedEntity= context.Entry(entity); //referans yakalama
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                //veri tabanında productın birini  bir entitye eşitle
+                var deletedEntity = context.Entry(entity); //referans yakalama
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            using (NorthwindContext context= new NorthwindContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return filter == null ? context.Set<Product>().ToList() : context.Set<Product>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                //veri tabanında productın birini  bir entitye eşitle
+                var uptadedEntity = context.Entry(entity); //referans yakalama
+                uptadedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
